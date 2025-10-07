9e2b339580717fac9e6df8af745c3ce0dc8503ed
Derek Johnson <howpopsir@gmail.com>
2025-10-06 20:22:07 -0400

    add character animation and sprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,14 @@ public class PlayerMovement : MonoBehaviour
     
     [SerializeField] float moveSpeed;
     private Rigidbody2D rb;
+    private Animator animator;
     Vector2 input;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -21,6 +22,21 @@ public class PlayerMovement : MonoBehaviour
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Vertical");
         input.Normalize();
+
+        if (input.y > 0 || input.y < 0 || input.x > 0 || input.x < 0) // RIGHT NOW WE ONLY HAVE UP AND DOWN ANIMATIONS
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", input.x);
+            animator.SetFloat("InputY", input.y);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("prevInputX", input.x);
+            animator.SetFloat("prevInputY", input.y);
+        }
+        
+        
     }
 
     void FixedUpdate()
