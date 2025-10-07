887bac4cb3a76860d306588d3107d461e1d0923a
Lukebonn <113559516+Lukebonn@users.noreply.github.com>
2025-10-07 12:34:20 -0400

    folder-management + levelmanagerv1

---
+++ Assets/Scripts/Core/LevelChanger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour {
+
+    [SerializeField]
+    private LevelConnection _connection;
+
+    [SerializeField]
+    private string _targetSceneName;
+
+    [SerializeField]
+    private Transform _spawnPoint;
+}
---
+++ Assets/Scripts/Core/LevelConnection.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Levels/Connections")]
+
+public class LevelConnection : ScriptableObject
+{
+    
+}
