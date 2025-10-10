256bc2d491a23bd09c7c8a6deef279f0ffc7f339
Lukebonn <113559516+Lukebonn@users.noreply.github.com>
2025-10-10 14:58:16 -0400

    SceneSwitch + PlayerIdentities fixes

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -5,6 +5,11 @@ public class SceneSwitch : MonoBehaviour
 {
     [SerializeField] string sceneName;
 
+    public void SetScene(string name) //allows this function to be called in separate files and not just in inspector in unity
+    {
+        sceneName = name;
+    }
+
     public void SwitchScene()
     {
         if (!string.IsNullOrEmpty(sceneName))
