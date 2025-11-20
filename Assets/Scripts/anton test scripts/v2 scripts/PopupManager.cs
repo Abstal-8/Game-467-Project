f9e985ab705a86a9a1939c87ce07824a2dbb57d3
Dlizard72 <arigoldring77@gmail.com>
2025-11-19 23:14:28 -0500

    bunch of stuff

--- Assets/Scripts/anton test scripts/v2 scripts/PopupManager.cs
+++ Assets/Scripts/anton test scripts/v2 scripts/PopupManager.cs
@@ -132,54 +132,25 @@ public class PopupManager : MonoBehaviour
     private void SetupChoice()
     {
         isChoosing = true;
-
-        // Move the dialogue panel left to make room (optional)
+        
         dialoguePanel.transform.localPosition = new Vector3(
-            dialoguePanel.transform.localPosition.x - 400,
-            dialoguePanel.transform.localPosition.y,
-            dialoguePanel.transform.localPosition.z
-        );
-
+        dialoguePanel.transform.localPosition.x - 400,
+        dialoguePanel.transform.localPosition.y,
+        dialoguePanel.transform.localPosition.z);
         choicePanel.SetActive(true);
 
-        // First, hide all buttons and clear old listeners
-        for (int b = 0; b < buttonChoices.Count; b++)
+        int index = 0;
+        int i = 0;
+
+        foreach(Choice choice in currentStory.currentChoices)
         {
-            buttonChoices[b].gameObject.SetActive(false);
-            buttonChoices[b].onClick.RemoveAllListeners();
-        }
-
-        // Now set up buttons for each current Ink choice
-        for (int i = 0; i < currentStory.currentChoices.Count; i++)
-        {
-            Choice choice = currentStory.currentChoices[i];
-
-            Button btn = buttonChoices[i];
-            btn.gameObject.SetActive(true);
-
-            TextMeshProUGUI buttonText = btn.GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = choice.text;
-
-            int choiceIndex = i; // capture i for this specific button
-
-            btn.onClick.AddListener(() =>
-            {
-                isChoosing = false;
-                choicePanel.SetActive(false);
-
-                // (optional) move dialogue panel back:
-                dialoguePanel.transform.localPosition = new Vector3(
-                    dialoguePanel.transform.localPosition.x + 400,
-                    dialoguePanel.transform.localPosition.y,
-                    dialoguePanel.transform.localPosition.z
-                );
-
-                currentStory.ChooseChoiceIndex(choiceIndex);
-                AdvanceDialogue();
-            });
+            buttonChoices[index].gameObject.SetActive(true);
+            TextMeshProUGUI buttontext = buttonChoices[index].gameObject.GetComponentInChildren<TextMeshProUGUI>();
+            buttontext.text = choice.text;
+            buttonChoices[index].onClick?.AddListener(() => {currentStory.ChooseChoiceIndex(i += 1); AdvanceDialogue();});
+            index++;
         }
     }
-
 
     IEnumerator TypeLine(string line)
     {
